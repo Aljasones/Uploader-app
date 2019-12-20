583ef1b705435367e00d86d0c8d934de7520000f
Aleksey <aljasones@yandex.ru>
2019-12-21 02:04:04 +0300

    add scheduler

---
+++ web-module/src/main/java/ru/itpark/sheduler/TaskScheduler.java
@@ -0,0 +1,32 @@
+package ru.itpark.sheduler;
+
+import ru.itpark.service.TaskService;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@WebListener
+public class TaskScheduler implements ServletContextListener {
+
+    private ScheduledExecutorService scheduler;
+    private TaskService taskService;
+
+    public TaskScheduler() {
+        taskService = new TaskService();
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        scheduler = Executors.newScheduledThreadPool(4);
+        scheduler.scheduleAtFixedRate(() ->  taskService.startTasksToWork(), 0, 10, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        scheduler.shutdownNow();
+    }
+}
